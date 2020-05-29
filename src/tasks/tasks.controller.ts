1fd42bea35419dc97d2de0b38d9e79459ee566d1
EdgardoArriagada <edgardo.arriagada@viajesfalabella.com>
2020-05-29 18:01:57 -0400

    service is able to filter tasks by query params now

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,18 +6,22 @@ import {
   Param,
   Delete,
   Patch,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (isEmptyObject(filterDto)) return this.tasksService.getAllTasks();
+
+    return this.tasksService.getTasksWithFilters(filterDto);
   }
 
   @Get('/:id')
@@ -43,3 +47,5 @@ export class TasksController {
     return this.tasksService.updateTaskStatus({ id, status });
   }
 }
+
+const isEmptyObject = (param: any): boolean => Object.keys(param).length === 0;
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,17 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters({ status, search }: GetTasksFilterDto): Task[] {
+    const tasks = this.getAllTasks()
+      .filter(task => task.status === status)
+      .filter(
+        task =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+
+    return tasks;
   }
 
   getTaskById(id: string): Task {
