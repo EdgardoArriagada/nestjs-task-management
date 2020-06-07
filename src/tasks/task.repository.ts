7ca3ffeade2bc81a5dd2b2030a0bb39ab9e58c71
EdgardoArriagada <edgardo.arriagada@viajesfalabella.com>
2020-06-07 17:22:53 -0400

    create task repository

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,5 +1,34 @@
 import { Task } from './task.entity';
 import { Repository, EntityRepository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @EntityRepository(Task)
-export class TaskRepository extends Repository<Task> {}
+export class TaskRepository extends Repository<Task> {
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+    const query = this.createQueryBuilder('task');
+
+    if (status) {
+      query.where('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.orWhere('task.title LIKE :search or task.description LIKE :search ', { search: `%${search}%` });
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
+  }
+
+  async createTask({ title, description }: CreateTaskDto): Promise<Task> {
+    const task = new Task();
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+    await task.save();
+
+    return task;
+  }
+}
