0a98140690cd5af7a5926afea349e365342b961a
EdgardoArriagada <edgardo.arriagada@viajesfalabella.com>
2020-06-08 12:39:32 -0400

    create auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+    return this.authService.signUp(authCredentialsDto);
+  }
+}
