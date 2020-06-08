bd201384c7ebbd56cf86d3869215db482c6206c7
EdgardoArriagada <edgardo.arriagada@viajesfalabella.com>
2020-06-08 13:40:21 -0400

    handle duplicate user

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,17 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+
+const saveUser = async (user: User) => {
+  const DUPLICATE_ERROR_CODE = '23505';
+  try {
+    await user.save();
+  } catch (error) {
+    if (error.code === DUPLICATE_ERROR_CODE) throw new ConflictException('Username already exists');
+    throw new InternalServerErrorException();
+  }
+};
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -11,6 +22,6 @@ export class UserRepository extends Repository<User> {
     user.username = username;
     user.password = password;
 
-    await user.save();
+    await saveUser(user);
   }
 }
