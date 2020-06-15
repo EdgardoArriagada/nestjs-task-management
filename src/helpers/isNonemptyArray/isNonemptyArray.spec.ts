6f4f972f69ae36f6fa7bd7f5faa9d09ddcbd9d9f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-06-15 14:14:53 -0400

    configure tests according to tutorial

--- src/helpers/isNonemptyArray/isNonemptyArray.spec.ts
+++ src/helpers/isNonemptyArray/isNonemptyArray.spec.ts
@@ -1,22 +1,22 @@
 import isNonemptyArray from './isNonemptyArray';
 
 describe('isNonemptyArray', () => {
-  describe('true for', () => {
-    it('should recognize number filled array', () => {
+  describe('returns true when:', () => {
+    it('non empty number array is given', () => {
       const input = [1, 2, 3, 4];
       expect(isNonemptyArray(input)).toBe(true);
     });
-    it('should recognize array with one element', () => {
+    it('non empty undefined array is given', () => {
       const input = [undefined];
       expect(isNonemptyArray(input)).toBe(true);
     });
   });
-  describe('false for', () => {
-    it('should false on empty array', () => {
+  describe('returns false when:', () => {
+    it('empty array is given', () => {
       const input = [];
       expect(isNonemptyArray(input)).toBe(false);
     });
-    it('should false on any number', () => {
+    it('number input is given', () => {
       const input = 1;
       expect(isNonemptyArray(input)).toBe(false);
     });
